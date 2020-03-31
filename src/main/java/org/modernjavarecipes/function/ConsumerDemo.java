66eba00650f77156d10b3781f6a3227e53f98404
Çetin İmre <ctn.imre@gmail.com>
2020-03-31 22:07:30 +0300

    ConsumerDemo

---
+++ src/main/java/org/modernjavarecipes/function/ConsumerDemo.java
@@ -0,0 +1,24 @@
+package org.modernjavarecipes.function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("this", "is", "a", "list", "of", "strings");
+        strings.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+        strings.forEach(s -> System.out.println(s));
+        strings.forEach(System.out::println);
+
+        //Stream.forEach(Consumer<? super T> action);
+        //Optional.ifPresent(Consumer<? super T> consumer)
+        //Stream.peek(Consumer<? super T> action)
+    }
+}
