6e868b9a6eb3508493bda26c5748de15b980eecc
Çetin İmre <ctn.imre@gmail.com>
2020-04-14 22:36:41 +0300

    src/main/java/org/modernjavarecipes/LambdasandEffectivelyFinalDemo.java

---
+++ src/main/java/org/modernjavarecipes/LambdasandEffectivelyFinalDemo.java
@@ -0,0 +1,21 @@
+package org.modernjavarecipes;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdasandEffectivelyFinalDemo {
+
+    public static void main(String[] args) {
+        //Sum the values in a List
+        List<Integer> nums = Arrays.asList(3, 1, 4, 1, 5, 9);
+        int total = 0;
+        for (int n : nums) {
+            total += n;
+        }
+        total = 0;
+        nums.forEach(n -> total += n);
+        total = nums.stream()
+                .mapToInt(Integer::valueOf)
+                .sum();
+    }
+}
