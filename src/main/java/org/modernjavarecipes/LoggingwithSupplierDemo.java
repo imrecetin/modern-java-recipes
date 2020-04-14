aabbbce4a249c3647ea410a2c4a45eebf8635a68
Çetin İmre <ctn.imre@gmail.com>
2020-04-14 22:36:57 +0300

    src/main/java/org/modernjavarecipes/LoggingwithSupplierDemo.java

---
+++ src/main/java/org/modernjavarecipes/LoggingwithSupplierDemo.java
@@ -0,0 +1,16 @@
+package org.modernjavarecipes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class LoggingwithSupplierDemo {
+    private static Logger logger = Logger.getLogger(this.getClass().getName());
+    public static void main(String[] args) {
+        //Using a Supplier in the info method
+        List<String> data = new ArrayList<>();
+        // ... populate list with data ...
+        logger.info("The data is " + data.toString());
+        logger.info(() -> "The data is " + data.toString());
+    }
+}
