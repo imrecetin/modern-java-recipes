de44418b0c8d17dfee7d332072d32e149431ec08
Çetin İmre <ctn.imre@gmail.com>
2020-04-20 12:56:17 +0300

    src/main/java/org/modernjavarecipes/theJavaTimePackage/FindingTimeZonesWithUnusualOffsetsDemo.java

---
+++ src/main/java/org/modernjavarecipes/theJavaTimePackage/FindingTimeZonesWithUnusualOffsetsDemo.java
@@ -0,0 +1,35 @@
+package org.modernjavarecipes.theJavaTimePackage;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import static java.util.Comparator.comparingInt;
+
+public class FindingTimeZonesWithUnusualOffsetsDemo {
+    public static void main(String[] args) {
+        //Funny Offset
+        Instant instant = Instant.now();
+        ZonedDateTime current = instant.atZone(ZoneId.systemDefault());
+        System.out.printf("Current time is %s%n%n", current);
+        System.out.printf("%10s %20s %13s%n", "Offset", "ZoneId", "Time");
+        ZoneId.getAvailableZoneIds().stream()
+                .map(ZoneId::of)
+                .filter(zoneId -> {
+                    ZoneOffset offset = instant.atZone(zoneId).getOffset();
+                    return offset.getTotalSeconds() % (60 * 60) != 0;
+                })
+                .sorted(comparingInt(zoneId ->
+                        instant.atZone(zoneId).getOffset().getTotalSeconds()))
+                .forEach(zoneId -> {
+                    ZonedDateTime zdt = current.withZoneSameInstant(zoneId);
+                    System.out.printf("%10s %25s %10s%n",
+                            zdt.getOffset(), zoneId,
+                            zdt.format(DateTimeFormatter.ofLocalizedTime(
+                                    FormatStyle.SHORT)));
+                });
+    }
+}
